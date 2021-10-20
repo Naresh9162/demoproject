2beffb4cb1a6a7e9ab9e48cbf1a5ae273c69af20
NaSharma <naresh.sharma@microfocus.com>
2021-10-20 10:04:38 +0530

    20/10/21

--- src/test/java/com/mavenproject/demoproject/program1.java
+++ src/test/java/com/mavenproject/demoproject/program1.java
@@ -3,7 +3,8 @@ package com.mavenproject.demoproject;
 public class program1 {
 	public static void main(String args[])
 	{
-	System.out.print("hello Naresh");
+	System.out.println("hello Naresh");
+	System.out.print("hi");
 
 }
 }
